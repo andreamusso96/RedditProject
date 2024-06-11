47c57e39e06584b43a40da4e52b6f58b7060e9b6
andreamusso96 <andreamusso96@gmail.com>
2024-06-11 18:40:12 +0200

    parquet scripts

--- Parquet/launch_script.py
+++ Parquet/launch_script.py
@@ -1,6 +1,8 @@
 from typing import List
 import os
 
+import pandas as pd
+
 
 def extract_zst_file_names(folder_path: str) -> List[str]:
     file_names = os.listdir(folder_path)
@@ -14,9 +16,12 @@ def launch_jobs():
     zst_folder_path = '/cluster/work/coss/anmusso/reddit/submissions'
     parquet_folder_path = '/cluster/work/coss/anmusso/reddit_parquet/submissions'
     file_names = extract_zst_file_names(folder_path=zst_folder_path)
+    file_names = sorted(file_names)[:1]
+    print(file_names)
 
     os.system('chmod +x run_script.sh')
     for file_name in file_names:
+
         os.system(f'sbatch run_script.sh {file_name} {zst_folder_path} {parquet_folder_path}')
         # os.system(f'./run_script.sh {file_name} {zst_folder_path} {parquet_folder_path}')
 
--- Parquet/run_script.sh
+++ Parquet/run_script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH --nodes=1
-#SBATCH --mem-per-cpu=2GB
+#SBATCH --mem-per-cpu=5GB
 #SBATCH --time=00:30:00
 
 module load gcc/8.2.0 python/3.11.2

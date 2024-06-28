67b5db7e5e04ece33d5fbd6351cc4d0f7b659662
andreamusso96 <andreamusso96@gmail.com>
2024-06-28 17:11:43 +0200

    parquet transform

--- Parquet/slurm_job_script.py
+++ Parquet/slurm_job_script.py
@@ -9,10 +9,15 @@ def get_file_string(file_path: str):
 def analyze_files():
     files = os.listdir('/Users/andrea/Desktop/Temp/slurm_jobs/')
     files = [f for f in files if f.endswith('.out')]
+    bad_files = []
     for f in files:
         s = get_file_string(f'/Users/andrea/Desktop/Temp/slurm_jobs/{f}')
-        if 'RS_2022' in s:
-            print(s)
+        if 'BAD LINES SHARE 1.00' in s:
+            file = s.split('\n')[1].split(' ')[8].split('/')[-1]
+            print(file)
+            bad_files.append(file)
+
+    print(sorted(bad_files))
 
 
 
